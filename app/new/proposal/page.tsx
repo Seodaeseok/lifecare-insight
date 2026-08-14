ec492e8cc0d114d02d9fe41d284bfe8cb218d735
Seodaeseok <a01020184086@gmail.com>
2026-08-14 15:52:35 +0900

    Update page.tsx

--- app/new/proposal/page.tsx
+++ app/new/proposal/page.tsx
@@ -87,10 +87,13 @@ export default function ProposalPage() {
     <main style={mainStyle}>
       {/* 상단 버튼 */}
       <div style={topBarStyle}>
-        <button onClick={() => router.push('/new/coverage')} style={navButtonStyle}>
+        <button 
+          onClick={() => router.push('/new/coverage')} 
+          style={navButtonStyle}>
           ← 보장분석
         </button>
-        <button onClick={() => router.push('/')} style={navButtonStyle}>
+        <button onClick={() => router.push('/')} 
+          style={navButtonStyle}>
           ⌂ 메인
         </button>
               </div>
